28f3980fbcec43c2a475cfdc5d860e580948fb0e
ChiomaUU <chiomaulom@gmail.com>
2025-03-20 04:47:09 -0600

    Update streamlit2_app.py

--- streamlit2_app.py
+++ streamlit2_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import joblib
 import os
-!pip install --upgrade pip
+python3 -m pip install --upgrade pip
 
 # Load the trained model with caching
 @st.cache_resource
